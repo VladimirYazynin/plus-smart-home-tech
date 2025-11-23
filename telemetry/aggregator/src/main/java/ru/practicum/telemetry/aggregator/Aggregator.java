39ddb7ca9ed743ead981df4b72511b5b75bf77c0
VladimirYazynin <vladimir.yazynin@inbox.ru>
2025-11-24 01:41:09 +0300

    Реализация сервиса Aggregator

--- telemetry/aggregator/src/main/java/ru/practicum/telemetry/aggregator/Aggregator.java
+++ telemetry/aggregator/src/main/java/ru/practicum/telemetry/aggregator/Aggregator.java
@@ -2,12 +2,16 @@ package ru.practicum.telemetry.aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.practicum.telemetry.aggregator.messaging.AggregationStarter;
 
 @SpringBootApplication
 public class Aggregator {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aggregator.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(Aggregator.class, args);
+        AggregationStarter aggregator = context.getBean(AggregationStarter.class);
+        aggregator.start();
     }
 
 }
