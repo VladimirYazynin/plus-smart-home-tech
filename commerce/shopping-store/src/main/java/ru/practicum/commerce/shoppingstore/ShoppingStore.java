00cd1fb79639990e6e60d91bd7201c96766bd32f
VladimirYazynin <vladimir.yazynin@inbox.ru>
2026-02-14 18:29:45 +0300

    Добавление заготовки для модуля commerce

---
+++ commerce/shopping-store/src/main/java/ru/practicum/commerce/shoppingstore/ShoppingStore.java
@@ -0,0 +1,12 @@
+package ru.practicum.commerce.shoppingstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingStore {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingStore.class, args);
+    }
+}
