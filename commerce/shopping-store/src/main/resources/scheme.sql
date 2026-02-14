665acb8ff437770ca2c08c22698b1e79e98ef8d4
VladimirYazynin <vladimir.yazynin@inbox.ru>
2026-02-14 21:10:20 +0300

    Добавление контейнера с базой и скрипта создания таблицы products для shopping-store

---
+++ commerce/shopping-store/src/main/resources/scheme.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS products (
+    product_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    product_name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    image_src VARCHAR(255),
+    quantity_state VARCHAR(20) NOT NULL,
+    product_state VARCHAR(20) NOT NULL,
+    product_category VARCHAR(20) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL CHECK (price >= 1)
+);
