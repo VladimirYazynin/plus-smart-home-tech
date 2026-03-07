2a8b19afec734cca93fd20ebf5a8f7e19ccb748e
VladimirYazynin <vladimir.yazynin@inbox.ru>
2026-03-07 17:29:54 +0300

    Добавление настроек сервиса gateway и подключение его к config-server и eureka

--- telemetry/collector/src/main/java/ru/practicum/telemetry/collector/CollectorApp.java
+++ telemetry/collector/src/main/java/ru/practicum/telemetry/collector/CollectorApp.java
@@ -2,8 +2,10 @@ package ru.practicum.telemetry.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CollectorApp {
 
     public static void main(String[] args) {
